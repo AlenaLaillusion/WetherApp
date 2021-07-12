4b12e9023716d7402d27429f15e5cee23787afc3
helen.dikovitsky@gmail.com <helen.dikovitsky@gmail.com>
2021-07-12 11:29:50 +0300

    commit_json_http

---
+++ lib/constant.dart
@@ -0,0 +1,14 @@
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const String WEATHER_BASE_SCHEM = 'https://';
+  static const String WEATHER_BASE_URL = 'api.openweathermap.org';
+  static const String WEATHER_IMAGES_PATH = "/img/w/";
+  static const String WEATHER_IMAGES_URL = WEATHER_BASE_SCHEM + WEATHER_BASE_URL + WEATHER_IMAGES_PATH;
+  static const String WEATHER_FORECAST_URL = "/data/2.5/forecast";
+  static const String WEATHER_APP_ID = "";
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:weather_app/heading.dart';
-import 'package:weather_app/weather.dart';
-import 'package:intl/date_symbol_data_local.dart';
+import 'constant.dart';
+import 'weather.dart';
+import 'model/forecast_response.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
 
 void main() {
   runApp(MyApp());
@@ -22,42 +28,79 @@ class WeatherForcecastPage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-  return _WeatherForcecastPageState();
+
+  return _WeatherForcecastPageState(35.0164, 139.0077);
   }
 }
 
 class _WeatherForcecastPageState extends State<WeatherForcecastPage> {
-  List<ListItem> weatherForcast = List<ListItem>();
+
+  final _skaffoldKey = GlobalKey<ScaffoldState>();
+
+  final double latitude;
+  final double longitude;
+
+  List<ListItem> weatherForcast;
+  _WeatherForcecastPageState(this.latitude, this.longitude);
+
+  Future<List<ListItem>> getWeather(double lat, double lng) async {
+    var queryParameters = { //подготавливаем параметры запроса
+      'appid': Constants.WEATHER_APP_ID,
+      'units': 'metric',
+      'lat': lat.toString(),
+      'lon': lng.toString(),
+    };
+    var uri = Uri.https(Constants.WEATHER_BASE_URL,
+       Constants.WEATHER_FORECAST_URL, queryParameters);
+
+    var response = await http.get(uri); //выполняем запрос и ждем результат
+    print(response.body);
+    print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      var forecastResponseEntity = ForecastResponse.fromMap(json.decode(response.body));
+      if ( forecastResponseEntity.cod == "200") {//в случае успеха парси Json и возвращаем список с прогнозом
+        print(forecastResponseEntity.list);
+        return forecastResponseEntity.list;
+      } else {
+        //в случае ошибки показываем ошибку
+        print("Error ${forecastResponseEntity.cod}");
+      }
+    } else {
+      //в случае ошибки показываем ошибку
+    print("Error occured while loading data from server");
+    }
+    return List<ListItem>();
+  }
+
   @override
   void initState() {
     var itCurrentDay = DateTime.now();
-    weatherForcast.add(DayHeading(itCurrentDay)); //first heading
-    List<ListItem> weatherData = [
-    Weather(itCurrentDay, 20, 90, "04d"),
-      Weather(itCurrentDay.add(Duration(hours: 3)), 23, 50, "03d"),
-      Weather(itCurrentDay.add(Duration(hours: 5)), 23, 55, "03d"),
-    Weather(itCurrentDay.add(Duration(hours: 6)), 25, 50, "02d"),
-    Weather(itCurrentDay.add(Duration(hours: 9)), 20, 60, "01d"),
-    Weather(itCurrentDay.add(Duration(hours: 17)), 24, 60, "02d"),
-    Weather(itCurrentDay.add(Duration(hours: 32)), 28, 50, "01d")
-    ];
-    var itNextDay = DateTime.now().add(Duration(days: 1));
-    itNextDay = DateTime(
-      itNextDay.year, itNextDay.month, itNextDay.day, 0, 0, 0, 0, 1);
-    var iterator = weatherData.iterator;
-    while (iterator.moveNext()) {
-      var weatherDateTime = iterator.current as Weather;
-      if(weatherDateTime.dateTime.isAfter(itNextDay)) {
-        itCurrentDay = itNextDay;
-        itNextDay = itCurrentDay.add(Duration(days: 1));
-        itNextDay = DateTime(
-          itNextDay.year, itNextDay.month, itNextDay.day, 0, 0, 0, 0, 1);
-        weatherForcast.add(DayHeading(itCurrentDay)); //next heading
-      } else {
-        weatherForcast.add(iterator.current);
-      }
-    }
-    super.initState();
+   var dataFuture = getWeather(latitude, longitude);
+   dataFuture.then((value) {
+     var weatherForecastLocal = List<ListItem>();
+     weatherForecastLocal.add(DayHeading(itCurrentDay)); // first heading
+     List<ListItem> weatherData = value;
+     var itNextDay = DateTime.now().add(Duration(days: 1));
+     itNextDay = DateTime(
+         itNextDay.year, itNextDay.month, itNextDay.day, 0, 0, 0, 0, 1);
+     var iterator = weatherData.iterator;
+     while (iterator.moveNext()) {
+       var weatherDateTime = iterator.current as WeatherListBean;
+       if (weatherDateTime.getDateTime().isAfter(itNextDay)) {
+         itCurrentDay = itNextDay;
+         itNextDay = itCurrentDay.add(Duration(days: 1));
+         itNextDay = DateTime(
+             itNextDay.year, itNextDay.month, itNextDay.day, 0, 0, 0, 0, 1);
+         weatherForecastLocal.add(DayHeading(itCurrentDay)); // next heading
+       } else {
+         weatherForecastLocal.add(iterator.current);
+       }
+     }
+       setState(() {
+       weatherForcast = weatherForecastLocal;
+   });
+   });
   }
 
   @override
@@ -72,10 +115,10 @@ class _WeatherForcecastPageState extends State<WeatherForcecastPage> {
           title: Text('Weather forecast'),
         ),
         body: ListView.builder(
-          itemCount: weatherForcast.length,
+          itemCount: weatherForcast == null ? 0 : weatherForcast.length,
             itemBuilder: (BuildContext context, int index) {
             final item = weatherForcast[index];
-            if (item is Weather) {
+            if (item is WeatherListBean) {
               return WeatherListItem(item);
             } else if(item is DayHeading) {
               return HeadingListItem(item);
@@ -84,6 +127,32 @@ class _WeatherForcecastPageState extends State<WeatherForcecastPage> {
             }
         )));
   }}
+
+/*
+Future<Placemark> getLocation() async {
+  Geolocator geolocator = Geolocator()..forceAndroidLocationManager = true;
+  Position position = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low); //получение геопозиции
+  List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude); //определение названия места
+  if(placemark.isNotEmpty) {
+    return placemark[0]; //возвращаем первый элемент из списка полученных вариантов
+  }
+  return null;
+}
+
+void _loadData() {
+  _isLoading = true;
+  var locationFuture = getLocation(); //получение future на геопозицию
+  locationFuture.then((placemark) { //берем значение из результата
+    var weatherFuture = getWeather(placemark.position.latitude, placemark.position.longitude); // делаем запрос на получение погоды
+    weatherFuture.then((weatherData) {
+      initWeatherWithData(weatherData, placemark);
+      _isLoading = false;
+    });
+  });
+}
+
+*/
+
 
 
 
